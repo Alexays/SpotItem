31f408bb0fe498c94fe034a8d45889e815dac07c
Alexis Rouillard <alexisr245@gmail.com>
2017-08-17 21:58:59 +0200

    feat(addGroup) add dialog

--- lib/ui/add_group_screen.dart
+++ lib/ui/add_group_screen.dart
@@ -51,91 +51,106 @@ class _AddGroupScreenState extends State<AddGroupScreen> {
           })
         ],
       ),
-      body: new SingleChildScrollView(
-          child: new Container(
-        margin: const EdgeInsets.all(20.0),
-        child: new Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            new Form(
-                key: _formKey,
-                child: new Column(
-                  children: <Widget>[
-                    new TextFormField(
-                      key: new Key('name'),
-                      decoration:
-                          new InputDecoration.collapsed(hintText: "Name"),
-                      autofocus: true,
-                      onSaved: (String value) {
-                        name = value;
-                      },
-                    ),
-                    new TextFormField(
-                      key: new Key('about'),
-                      decoration: new InputDecoration.collapsed(
-                          hintText: 'Description'),
-                      onSaved: (String value) {
-                        about = value;
-                      },
-                    ),
-                    new Divider(),
-                    new Row(
-                      children:
-                          new List<Widget>.generate(email.length, (int index) {
-                        return new Chip(
-                          avatar: new CircleAvatar(
-                            backgroundColor: Colors.grey.shade800,
-                            child: new Text('AB'),
-                          ),
-                          label: new Text('Aaron Burr'),
-                        );
-                      }),
-                    ),
-                    new RaisedButton(
-                      child: const Text("Add someone"),
-                      onPressed: () {
-                        String _email;
-                        showDialog<Null>(
-                          context: context,
-                          barrierDismissible: false, // user must tap button!
-                          child: new AlertDialog(
-                            title: new Text('Add someone'),
-                            content: new SingleChildScrollView(
-                              child: new ListBody(
-                                children: <Widget>[
-                                  new Text('Enter email of user.'),
-                                  new TextFormField(
-                                    key: new Key('email'),
-                                    decoration: new InputDecoration.collapsed(
-                                        hintText: 'ex: john.do@exemple.com'),
-                                    onSaved: (String value) {
-                                      _email = value;
-                                    },
-                                  ),
-                                ],
-                              ),
+      body: new Builder(builder: (BuildContext context) {
+        return new SingleChildScrollView(
+            child: new Container(
+          margin: const EdgeInsets.all(20.0),
+          child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Form(
+                  key: _formKey,
+                  child: new Column(
+                    children: <Widget>[
+                      new TextFormField(
+                        key: new Key('name'),
+                        decoration:
+                            new InputDecoration.collapsed(hintText: "Name"),
+                        autofocus: true,
+                        onSaved: (String value) {
+                          name = value;
+                        },
+                      ),
+                      new TextFormField(
+                        key: new Key('about'),
+                        decoration: new InputDecoration.collapsed(
+                            hintText: 'Description'),
+                        onSaved: (String value) {
+                          about = value;
+                        },
+                      ),
+                      new Divider(),
+                      new Row(
+                        children: new List<Widget>.generate(email.length,
+                            (int index) {
+                          return new Chip(
+                            avatar: new CircleAvatar(
+                              backgroundColor: Colors.grey.shade800,
+                              child: new Text('AB'),
                             ),
-                            actions: <Widget>[
-                              new FlatButton(
-                                child: new Text('Add'),
-                                onPressed: () {
-                                  final RegExp emailExp = new RegExp(
-                                      r'^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/');
-                                  if (!emailExp.hasMatch(_email))
-                                    email.add(_email);
-                                  Navigator.of(context).pop();
-                                },
+                            label: new Text('Aaron Burr'),
+                          );
+                        }),
+                      ),
+                      new RaisedButton(
+                        child: const Text("Add someone"),
+                        onPressed: () {
+                          String _email;
+                          showDialog<Null>(
+                            context: context,
+                            barrierDismissible: false, // user must tap button!
+                            child: new AlertDialog(
+                              title: new Text('Add someone'),
+                              content: new SingleChildScrollView(
+                                child: new ListBody(
+                                  children: <Widget>[
+                                    new Text('Enter email of user.'),
+                                    new TextFormField(
+                                      key: new Key('email'),
+                                      decoration: new InputDecoration.collapsed(
+                                          hintText: 'ex: john.do@exemple.com'),
+                                      onSaved: (String value) {
+                                        _email = value;
+                                      },
+                                    ),
+                                  ],
+                                ),
                               ),
-                            ],
-                          ),
-                        );
-                      },
-                    )
-                  ],
-                )),
-          ],
-        ),
-      )),
+                              actions: <Widget>[
+                                new FlatButton(
+                                  child: new Text('Cancel'),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                                new FlatButton(
+                                  child: new Text('Add'),
+                                  onPressed: () {
+                                    final RegExp emailExp = new RegExp(
+                                        r'^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/');
+                                    if (_email != null &&
+                                        !emailExp.hasMatch(_email)) {
+                                      email.add(_email);
+                                      Navigator.of(context).pop();
+                                    } else {
+                                      Scaffold.of(context).showSnackBar(
+                                          new SnackBar(
+                                              content: new Text(
+                                                  "Enter valid email")));
+                                    }
+                                  },
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      )
+                    ],
+                  )),
+            ],
+          ),
+        ));
+      }),
     );
   }
 }
