81924fb1cb0f3b4a9cf29d725aedf62cf54c51c8
Alexis Rouillard <alexisr245@gmail.com>
2017-08-25 09:29:39 +0200

    style() annotate headers

--- lib/keys.dart
+++ lib/keys.dart
@@ -3,5 +3,5 @@ const String clientSecret = 'et+nWhUB>.Dg[c4z';
 const String apiUrl = 'http://217.182.65.67:1337/api';
 const String apiImgUrl = 'http://217.182.65.67:1337/img/';
 const String staticApiKey = 'AIzaSyAJh3ASTwUBo06fQai_PZJa-R9czeRC2D0';
-Map<String, String> getHeaders([key = 'Basic $clientSecret']) =>
+Map<String, String> getHeaders([String key = 'Basic $clientSecret']) =>
     {'Authorization': key, 'accept-version': version};
