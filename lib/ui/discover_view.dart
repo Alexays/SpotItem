8502d55b4a1ec2bd57d89188c7814f20f6a1c39f
Alexis Rouillard <alexisr245@gmail.com>
2017-08-27 16:37:37 +0200

    fix(discover) don't show groups if items list is empty

--- lib/ui/discover_view.dart
+++ lib/ui/discover_view.dart
@@ -58,20 +58,10 @@ class _DiscoverViewState extends State<DiscoverView> {
   }
 
   Widget _buildDiscover() => new ListView.builder(
-      itemCount: 4,
+      itemCount: 2,
       itemBuilder: (context, index) {
         switch (index) {
           case 0:
-            return const Padding(
-              padding:
-                  const EdgeInsets.only(left: 10.0, top: 10.0, bottom: 0.0),
-              child: const Text(
-                'Recents items',
-                style: const TextStyle(
-                    fontWeight: FontWeight.w400, fontSize: 20.0),
-              ),
-            );
-          case 1:
             List<Item> recents = new List<Item>.from(_items);
             recents = recents
                 .where((item) => !item.tracks.contains('group'))
@@ -79,34 +69,55 @@ class _DiscoverViewState extends State<DiscoverView> {
             if (recents.length > 10) {
               recents.length = 10;
             }
-            return new Container(
-              height: 250.0,
-              width: MediaQuery.of(context).size.width,
-              child: new ItemsList(recents, _itemsManager, _authManager,
-                  'recents', Axis.horizontal),
+            return new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Padding(
+                  padding:
+                      const EdgeInsets.only(left: 10.0, top: 10.0, bottom: 0.0),
+                  child: const Text(
+                    'Recents items',
+                    style: const TextStyle(
+                        fontWeight: FontWeight.w400, fontSize: 20.0),
+                  ),
+                ),
+                new Container(
+                  height: 250.0,
+                  width: MediaQuery.of(context).size.width,
+                  child: new ItemsList(recents, _itemsManager, _authManager,
+                      'recents', Axis.horizontal),
+                ),
+              ],
             );
-          case 2:
-            return const Padding(
-              padding:
-                  const EdgeInsets.only(left: 10.0, top: 10.0, bottom: 0.0),
-              child: const Text(
-                'From your groups',
-                style: const TextStyle(
-                    fontWeight: FontWeight.w400, fontSize: 20.0),
-              ),
-            );
-          case 3:
+          case 1:
             List<Item> groups = new List<Item>.from(_items);
             groups =
                 groups.where((item) => item.tracks.contains('group')).toList();
+            if (groups.isEmpty) {
+              return new Container();
+            }
             if (groups.length > 10) {
               groups.length = 10;
             }
-            return new Container(
-              height: 250.0,
-              width: MediaQuery.of(context).size.width,
-              child: new ItemsList(groups, _itemsManager, _authManager, 'group',
-                  Axis.horizontal),
+            return new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Padding(
+                  padding:
+                      const EdgeInsets.only(left: 10.0, top: 10.0, bottom: 0.0),
+                  child: const Text(
+                    'From your groups',
+                    style: const TextStyle(
+                        fontWeight: FontWeight.w400, fontSize: 20.0),
+                  ),
+                ),
+                new Container(
+                  height: 250.0,
+                  width: MediaQuery.of(context).size.width,
+                  child: new ItemsList(groups, _itemsManager, _authManager,
+                      'group', Axis.horizontal),
+                )
+              ],
             );
         }
       });
