c3997bfaab3e60e014845cf0cffe23b27dded4e8
Alexays <alexisr245@gmail.com>
2017-09-03 15:53:57 +0200

    style() test color

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -10,11 +10,28 @@ class SpotItemApp extends MaterialApp {
       : super(
           title: 'SpotItem',
           home: init ? const HomeScreen() : const LoginScreen(),
-          theme: _spotTheme,
+          theme: new ThemeData(
+              primarySwatch: _spotTheme(),
+              accentColor: const Color(0xFF06A6D2),
+              scaffoldBackgroundColor: Colors.white,
+              primaryColor: _spotTheme(),
+              backgroundColor: Colors.white),
           onGenerateRoute: Services.router.generator,
           showPerformanceOverlay: false,
         );
 }
 
-final _spotTheme =
-    new ThemeData(primarySwatch: Colors.pink, accentColor: Colors.pinkAccent);
+MaterialColor _spotTheme() {
+  return new MaterialColor(0xFF0498C1, {
+    50: const Color(0xFFE1F3F8),
+    100: const Color(0xFFB4E0EC),
+    200: const Color(0xFF82CCE0),
+    300: const Color(0xFF4FB7D4),
+    400: const Color(0xFF2AA7CA),
+    500: const Color(0xFF0498C1),
+    600: const Color(0xFF0390BB),
+    700: const Color(0xFF0385B3),
+    800: const Color(0xFF027BAB),
+    900: const Color(0xFF016A9E)
+  });
+}
