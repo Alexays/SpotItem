06bbeb37a03a396a071bc6e5daf96edb8639d2df
Alexays <alexisr245@gmail.com>
2017-09-04 17:25:45 +0200

    refactor() move handleWsData to self function

--- lib/services/basic.dart
+++ lib/services/basic.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
+import 'dart:convert';
 import 'package:spotitem/keys.dart';
 import 'package:spotitem/services/services.dart';
-import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:web_socket_channel/io.dart';
 
@@ -20,12 +20,14 @@ class BasicService {
     Services.auth.oauthToken = oauthToken;
   }
 
+  void handleWsData(data) {
+    print(data);
+  }
+
   void connectWs() {
     final channel = new IOWebSocketChannel.connect('ws://217.182.65.67:1337');
     channel.sink.add(
         JSON.encode({'type': 'CONNECTION', 'userId': Services.auth.user.id}));
-    channel.stream.listen((message) {
-      print(message);
-    });
+    channel.stream.listen(handleWsData);
   }
 }
