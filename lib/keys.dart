509d6a48173b7136766bc7309a3c513439227924
Alexays <alexisr245@gmail.com>
2017-09-06 10:24:02 +0200

    feat(api) add spotkey

--- lib/keys.dart
+++ lib/keys.dart
@@ -1,10 +1,13 @@
 const String keyUser = 'KEY_USER';
 const String keyOauthToken = 'KEY_AUTH_TOKEN';
 const String keyProvider = 'KEY_AUTH_PROVIDER';
-const String version = '0.0.5';
+const String version = '0.1.0';
 const String clientSecret = 'et+nWhUB>.Dg[c4z';
 const String apiUrl = 'http://217.182.65.67:1337/api';
 const String apiImgUrl = 'http://217.182.65.67:1337/img/';
 const String staticApiKey = 'AIzaSyAJh3ASTwUBo06fQai_PZJa-R9czeRC2D0';
-Map<String, String> getHeaders([String key = 'Basic $clientSecret']) =>
-    {'Authorization': key, 'accept-version': version};
+Map<String, String> getHeaders([String key]) => {
+      'Authorization': key,
+      'spotkey': 'Basic $clientSecret',
+      'accept-version': version
+    };
