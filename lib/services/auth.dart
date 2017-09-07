f21e437be74d4aa03360f4e039afb48f1816cb7c
Alexays <alexisr245@gmail.com>
2017-09-07 22:13:22 +0200

    feat() logout on api side

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -152,6 +152,7 @@ class AuthManager extends BasicService {
     if (provider == 'google') {
       await _googleSignIn.signOut();
     }
+    await iget('/logout', accessToken);
     await saveTokens(null, null, null);
     _loggedIn = false;
   }
