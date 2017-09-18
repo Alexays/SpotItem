b96ae7d0e714f7f8be9701633d66432861a7548a
Alexays <alexisr245@gmail.com>
2017-09-18 21:49:04 +0200

    style() add padding login screen

--- lib/ui/screens/login_screen.dart
+++ lib/ui/screens/login_screen.dart
@@ -49,92 +49,99 @@ class _LoginScreenState extends State<LoginScreen> {
                       height: MediaQuery.of(context).size.height * 0.25,
                     ),
                   ),
-                  new Form(
-                    key: _formKey,
-                    child: new Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        new TextFormField(
-                          key: const Key('email'),
-                          decoration: const InputDecoration(
+                  new Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                    child: new Form(
+                      key: _formKey,
+                      child: new Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          new TextFormField(
+                            key: const Key('email'),
+                            decoration: const InputDecoration(
                               hintText: 'Enter email',
                               labelText: 'Email',
-                              icon: const Icon(Icons.email)),
-                          autofocus: true,
-                          controller: _usernameCtrl,
-                          validator: validateEmail,
-                          keyboardType: TextInputType.emailAddress,
-                        ),
-                        new TextFormField(
-                          key: const Key('password'),
-                          decoration: const InputDecoration(
+                            ),
+                            autofocus: true,
+                            controller: _usernameCtrl,
+                            validator: validateEmail,
+                            keyboardType: TextInputType.emailAddress,
+                          ),
+                          new TextFormField(
+                            key: const Key('password'),
+                            decoration: const InputDecoration(
                               hintText: 'Enter password',
                               labelText: 'Password',
-                              icon: const Icon(Icons.lock_outline)),
-                          controller: _passwordCtrl,
-                          obscureText: true,
-                          validator: validatePassword,
-                        ),
-                        new Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            new RaisedButton(
-                                key: const Key('login'),
-                                child: const Text('Login'),
-                                onPressed: () {
-                                  final FormState form = _formKey.currentState;
-                                  if (form.validate()) {
-                                    doLogin(context);
-                                  } else {
-                                    showSnackBar(
-                                        context, 'Form must be valid !');
-                                  }
-                                }),
-                            const Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 5.0)),
-                            new RaisedButton(
-                              child: const Text(
-                                'Google',
-                                style: const TextStyle(color: Colors.white),
-                              ),
-                              color: Colors.blue,
-                              onPressed: () {
-                                Services.auth
-                                    .handleGoogleSignIn()
-                                    .then((success) {
-                                  if (success) {
-                                    Navigator.pushReplacementNamed(
-                                        context, '/');
-                                  } else {
-                                    showSnackBar(context, 'Error !');
-                                  }
-                                });
-                              },
-                            )
-                          ],
-                        ),
-                        const Padding(padding: const EdgeInsets.all(5.0)),
-                        new Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            const Text('Don\'t have an account ?'),
-                            const Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 5.0)),
-                            new FlatButton(
-                              child: const Text(
-                                'Register',
-                                textAlign: TextAlign.center,
-                              ),
-                              onPressed: () {
-                                Navigator.pushReplacementNamed(
-                                    context, '/register');
-                              },
                             ),
-                          ],
-                        )
-                      ],
+                            controller: _passwordCtrl,
+                            obscureText: true,
+                            validator: validatePassword,
+                          ),
+                          const Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 5.0)),
+                          new Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              new RaisedButton(
+                                  key: const Key('login'),
+                                  child: const Text('Login'),
+                                  onPressed: () {
+                                    final FormState form =
+                                        _formKey.currentState;
+                                    if (form.validate()) {
+                                      doLogin(context);
+                                    } else {
+                                      showSnackBar(
+                                          context, 'Form must be valid !');
+                                    }
+                                  }),
+                              const Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 5.0)),
+                              new RaisedButton(
+                                child: const Text(
+                                  'Google',
+                                  style: const TextStyle(color: Colors.white),
+                                ),
+                                color: Colors.blue,
+                                onPressed: () {
+                                  Services.auth
+                                      .handleGoogleSignIn()
+                                      .then((success) {
+                                    if (success) {
+                                      Navigator.pushReplacementNamed(
+                                          context, '/');
+                                    } else {
+                                      showSnackBar(context, 'Error !');
+                                    }
+                                  });
+                                },
+                              )
+                            ],
+                          ),
+                          const Padding(padding: const EdgeInsets.all(5.0)),
+                          new Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              const Text('Don\'t have an account ?'),
+                              const Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 5.0)),
+                              new FlatButton(
+                                child: const Text(
+                                  'Register',
+                                  textAlign: TextAlign.center,
+                                ),
+                                onPressed: () {
+                                  Navigator.pushReplacementNamed(
+                                      context, '/register');
+                                },
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ],
