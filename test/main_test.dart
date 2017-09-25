36fa321bd46c800175c1e7ee292bbfae01b735a7
Alexays <alexisr245@gmail.com>
2017-09-25 16:31:25 +0200

    feat(test) able to see items

--- test/main_test.dart
+++ test/main_test.dart
@@ -12,38 +12,81 @@ void main() {
   if (binding is LiveTestWidgetsFlutterBinding)
     binding.framePolicy = LiveTestWidgetsFlutterBindingFramePolicy.fullyLive;
 
-  testWidgets('App launch, show login, login, show discover', (tester) async {
-    final mock = new ApiRes({
-      'success': true,
-      'data': {
-        'user': {
-          '_id': '1234567890',
-          'name': 'mock',
-          'firstname': 'mock',
-          'email': 'mock@spotitem.fr'
-        },
-        'exp': new DateTime.now().millisecondsSinceEpoch + 3000,
-        'access_token': 'Bearer mock',
-        'refresh_token': 'mock'
-      }
-    }, 200);
-    await Services.setup(Origin.mock, mock);
-    await tester.pumpWidget(new SpotItemApp(init: true));
-    await tester.pump(); // see https://github.com/flutter/flutter/issues/1865
-    await tester.pump(); // triggers a frame
+  group('Basic', () {
+    testWidgets('Login appear', (tester) async {
+      await Services.setup(Origin.mock);
+      await tester.pumpWidget(new SpotItemApp(init: true));
+      await tester.pump(); // see https://github.com/flutter/flutter/issues/1865
+      await tester.pump(); // triggers a frame
 
-    expect(find.byKey(const Key('email')), findsOneWidget);
+      expect(find.byKey(const Key('email')), findsOneWidget);
 
-    expect(find.text('Login'), findsOneWidget);
+      expect(find.text('Login'), findsOneWidget);
+    });
 
-    await tester.enterText(find.byKey(const Key('email')), 'test@example.com');
-    await tester.pump();
-    await tester.enterText(find.byKey(const Key('password')), '123456789A');
-    await tester.pump();
+    testWidgets('Able to login and show Home', (tester) async {
+      Services.mock = new ApiRes({
+        'success': true,
+        'data': {
+          'user': {
+            '_id': '1234567890',
+            'name': 'mock',
+            'firstname': 'mock',
+            'email': 'mock@spotitem.fr'
+          },
+          'exp': new DateTime.now().millisecondsSinceEpoch + 3000,
+          'access_token': 'Bearer mock',
+          'refresh_token': 'mock'
+        }
+      }, 200);
+      await tester.pumpWidget(new SpotItemApp(init: true));
+      await tester.pump();
+      await tester.pump();
 
-    await tester.longPress(find.byKey(const Key('login')));
-    await tester.pump();
-    await tester.pump();
-    expect(find.text('Discover'), findsOneWidget);
+      await tester.enterText(
+          find.byKey(const Key('email')), 'test@example.com');
+      await tester.pump();
+      await tester.enterText(find.byKey(const Key('password')), '123456789A');
+      await tester.pump();
+
+      await tester.longPress(find.byKey(const Key('login')));
+      await tester.pump();
+      await tester.pump();
+      expect(find.text('Discover'), findsOneWidget);
+    });
+
+    testWidgets('Show discover', (tester) async {
+      Services.mock = new ApiRes({
+        'success': true,
+        'data': [
+          {
+            '_id': '59c40cadc4de467318e0cc23',
+            'updatedAt': '2017-09-21T20:04:05.156Z',
+            'createdAt': '2017-09-21T19:02:05.242Z',
+            'name': 'Magnifique Nutella',
+            'about': '40% de noisettes  !!',
+            'owner': {
+              '_id': '596b7aa3c68d586d9b0d4bb8',
+              'email': 'ptitpilou44@gmail.com',
+              'name': 'Leray',
+              'firstname': 'Pierre'
+            },
+            'lat': 47.2199094,
+            'lng': -1.6881127,
+            'location': 'Couëron-les-bains',
+            'groups': [],
+            'tracks': [],
+            'calendar': [],
+            'last_geo': '2017-09-21T19:02:05.240Z',
+            'images': ['image_6ea936bade56ea9388fcbb76265ef011933f4e2d.jpg']
+          }
+        ]
+      }, 200);
+      await tester.pumpWidget(new SpotItemApp(init: true));
+      await tester.pump();
+      await tester.pump();
+      expect(find.text('Discover'), findsOneWidget);
+      expect(find.text('Magnifique Nutella'), findsOneWidget);
+    });
   });
 }
