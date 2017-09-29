59f832b8a0d833c04c442d3b252c9fec0485e344
Alexays <alexisr245@gmail.com>
2017-09-29 12:11:07 +0200

    feat(test) add sort test

--- test/main_test.dart
+++ test/main_test.dart
@@ -3,12 +3,12 @@ import 'package:spotitem/ui/app.dart';
 import 'package:spotitem/models/api.dart';
 import 'package:spotitem/services/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:spotitem/ui/widgets/item.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   SharedPreferences.setMockInitialValues({});
-  final TestWidgetsFlutterBinding binding =
-      TestWidgetsFlutterBinding.ensureInitialized();
+  final TestWidgetsFlutterBinding binding = TestWidgetsFlutterBinding.ensureInitialized();
   if (binding is LiveTestWidgetsFlutterBinding)
     binding.framePolicy = LiveTestWidgetsFlutterBindingFramePolicy.fullyLive;
 
@@ -16,12 +16,7 @@ void main() {
     final mockUser = new ApiRes({
       'success': true,
       'data': {
-        'user': {
-          '_id': '1234567890',
-          'name': 'mock name',
-          'firstname': 'mock firstname',
-          'email': 'mock@spotitem.fr'
-        },
+        'user': {'_id': '1234567890', 'name': 'mock name', 'firstname': 'mock firstname', 'email': 'mock@spotitem.fr'},
         'exp': new DateTime.now().millisecondsSinceEpoch + 3000,
         'access_token': 'Bearer mock',
         'refresh_token': 'mock'
@@ -36,17 +31,28 @@ void main() {
           'createdAt': '2017-09-21T19:02:05.242Z',
           'name': 'Magnifique Nutella',
           'about': '40% de noisettes  !!',
-          'owner': {
-            '_id': '1234567890',
-            'email': 'mock@spotitem.fr',
-            'name': 'mock',
-            'firstname': 'mock'
-          },
+          'owner': {'_id': '1234567890', 'email': 'mock@spotitem.fr', 'name': 'mock', 'firstname': 'mock'},
           'lat': 47.2199094,
           'lng': -1.6881127,
           'location': 'Couëron-les-bains',
           'groups': [],
           'tracks': ['gift'],
+          'calendar': [],
+          'last_geo': '2017-09-21T19:02:05.240Z',
+          'images': ['image_6ea936bade56ea9388fcbb76265ef011933f4e2d.jpg']
+        },
+        {
+          '_id': '59c40cadc4df467318e0cc23',
+          'updatedAt': '2017-09-21T20:04:05.156Z',
+          'createdAt': '2017-09-21T19:02:05.242Z',
+          'name': 'Amande',
+          'about': '20% de noisettes  !!',
+          'owner': {'_id': '1234567890', 'email': 'mock2@spotitem.fr', 'name': 'mock2', 'firstname': 'mock2'},
+          'lat': 27.2199094,
+          'lng': -1.6881127,
+          'location': 'Couëron-les-bains',
+          'groups': [],
+          'tracks': ['group'],
           'calendar': [],
           'last_geo': '2017-09-21T19:02:05.240Z',
           'images': ['image_6ea936bade56ea9388fcbb76265ef011933f4e2d.jpg']
@@ -70,8 +76,7 @@ void main() {
       await tester.pump();
       await tester.pump();
 
-      await tester.enterText(
-          find.byKey(const Key('email')), 'test@example.com');
+      await tester.enterText(find.byKey(const Key('email')), 'test@example.com');
       await tester.pump();
       await tester.enterText(find.byKey(const Key('password')), '123456789A');
       await tester.pump();
@@ -82,14 +87,16 @@ void main() {
       expect(find.text('Discover'), findsOneWidget);
     });
 
-    testWidgets('Show discover', (tester) async {
+    testWidgets('Show discover and group item', (tester) async {
       Services.mock = mockItems;
       await tester.pumpWidget(new SpotItemApp(init: true));
       await tester.pump();
       await tester.pump();
       expect(find.text('Discover'), findsOneWidget);
+      expect(find.text('From your groups'), findsOneWidget);
       expect(find.text('Magnifique Nutella'), findsOneWidget);
-      expect(Services.items.items.length, 1);
+      expect(find.text('Amande'), findsOneWidget);
+      expect(Services.items.items.length, 2);
     });
 
     testWidgets('Show explorer', (tester) async {
@@ -98,11 +105,74 @@ void main() {
       await tester.pump();
       await tester.pump();
       expect(find.text('Discover'), findsOneWidget);
-      await tester.tap(find.descendant(
-          of: find.byType(TabBar), matching: find.text('Explore')));
+      await tester.tap(find.descendant(of: find.byType(TabBar), matching: find.text('Explore')));
       await tester.pumpAndSettle();
       expect(find.text('Magnifique Nutella'), findsOneWidget);
-      expect(Services.items.items.length, 1);
+      expect(Services.items.items.length, 2);
+    });
+
+    testWidgets('Sort item by name', (tester) async {
+      Services.mock = mockItems;
+      await tester.pumpWidget(new SpotItemApp(init: true));
+      await tester.pump();
+      await tester.pump();
+      expect(find.text('Discover'), findsOneWidget);
+      await tester.tap(find.descendant(of: find.byType(TabBar), matching: find.text('Explore')));
+      await tester.pumpAndSettle();
+      expect(find.text('Magnifique Nutella'), findsOneWidget);
+      expect(find.text('Amande'), findsOneWidget);
+      expect(find.descendant(of: find.byType(ItemsListItem).first, matching: find.text('Magnifique Nutella')),
+          findsOneWidget);
+      await tester.tap(find.byType(PopupMenuButton));
+      await tester.pumpAndSettle();
+      await tester.tap(find.descendant(of: find.byType(PopupMenuItem), matching: find.text('Name')));
+      await tester.pumpAndSettle();
+      expect(find.descendant(of: find.byType(ItemsListItem).first, matching: find.text('Amande')), findsOneWidget);
+      expect(Services.items.items.length, 2);
+    });
+
+    testWidgets('Sort item by gift', (tester) async {
+      Services.mock = mockItems;
+      await tester.pumpWidget(new SpotItemApp(init: true));
+      await tester.pump();
+      await tester.pump();
+      expect(find.text('Discover'), findsOneWidget);
+      await tester.tap(find.descendant(of: find.byType(TabBar), matching: find.text('Explore')));
+      await tester.pumpAndSettle();
+      expect(find.text('Magnifique Nutella'), findsOneWidget);
+      expect(find.text('Amande'), findsOneWidget);
+      expect(find.descendant(of: find.byType(ItemsListItem).first, matching: find.text('Amande')), findsOneWidget);
+      await tester.tap(find.byIcon(Icons.filter_list));
+      await tester.pumpAndSettle();
+      await tester.tap(find.descendant(of: find.byType(SwitchListTile), matching: find.text('Gift')));
+      await tester.pumpAndSettle();
+      expect(find.text('Amande'), findsNothing);
+      expect(find.text('Magnifique Nutella'), findsOneWidget);
+      expect(Services.items.items.length, 2);
+      await tester.tap(find.descendant(of: find.byType(SwitchListTile), matching: find.text('Gift')));
+      await tester.pumpAndSettle();
+    });
+
+    testWidgets('Sort item by group', (tester) async {
+      Services.mock = mockItems;
+      await tester.pumpWidget(new SpotItemApp(init: true));
+      await tester.pump();
+      await tester.pump();
+      expect(find.text('Discover'), findsOneWidget);
+      await tester.tap(find.descendant(of: find.byType(TabBar), matching: find.text('Explore')));
+      await tester.pumpAndSettle();
+      expect(find.text('Magnifique Nutella'), findsOneWidget);
+      expect(find.text('Amande'), findsOneWidget);
+      expect(find.descendant(of: find.byType(ItemsListItem).first, matching: find.text('Amande')), findsOneWidget);
+      await tester.tap(find.byIcon(Icons.filter_list));
+      await tester.pumpAndSettle();
+      await tester.tap(find.descendant(of: find.byType(SwitchListTile), matching: find.text('From your groups')));
+      await tester.pumpAndSettle();
+      expect(find.text('Amande'), findsOneWidget);
+      expect(find.text('Magnifique Nutella'), findsNothing);
+      expect(Services.items.items.length, 2);
+      await tester.tap(find.descendant(of: find.byType(SwitchListTile), matching: find.text('From your groups')));
+      await tester.pumpAndSettle();
     });
 
     testWidgets('Show item page', (tester) async {
@@ -133,16 +203,14 @@ void main() {
       await tester.pumpWidget(new SpotItemApp(init: true));
       await tester.pump();
       await tester.pump();
-      await tester.tap(find.descendant(
-          of: find.byType(BottomNavigationBar), matching: find.text('Items')));
+      await tester.tap(find.descendant(of: find.byType(BottomNavigationBar), matching: find.text('Items')));
       await tester.pumpAndSettle();
       expect(find.text('Magnifique Nutella'), findsOneWidget); // View item
-      expect(
-          find.descendant(
-              of: find.byType(FloatingActionButton),
-              matching: find.icon(Icons.add)),
+      expect(find.text('Amande'), findsOneWidget); // View item
+      expect(find.descendant(of: find.byType(FloatingActionButton), matching: find.icon(Icons.add)),
           findsOneWidget); // View fab add item
       expect(find.byIcon(Icons.card_giftcard), findsOneWidget);
+      expect(find.byIcon(Icons.group), findsOneWidget);
     });
 
     testWidgets('I able to view my user information in drawer', (tester) async {
@@ -181,10 +249,7 @@ void main() {
       await tester.tap(find.byIcon(Icons.settings));
       await tester.pumpAndSettle();
       expect(find.text('Settings'), findsOneWidget);
-      expect(
-          find.text(
-              'Maximal distance: ${Services.settings.value.maxDistance}km'),
-          findsOneWidget);
+      expect(find.text('Maximal distance: ${Services.settings.value.maxDistance}km'), findsOneWidget);
     });
   });
 }
